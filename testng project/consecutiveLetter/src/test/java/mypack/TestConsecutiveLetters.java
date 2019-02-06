d20995009ebca87cda2b51d4d7785841871f3078
dhruvdp7 <46739476+dhruvdp7@users.noreply.github.com>
2019-02-06 07:45:35 +0530

    TestConsecutiveLetters file updated
    A new test case is been added

--- testng project/consecutiveLetter/src/test/java/mypack/TestConsecutiveLetters.java
+++ testng project/consecutiveLetter/src/test/java/mypack/TestConsecutiveLetters.java
@@ -1,13 +1,13 @@
 package mypack;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import java.util.*;
 import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
-
-
-
 public class TestConsecutiveLetters {
 	
+//Test Case 1
 	
 	@Test
 	public void testForPositiveScenario() {
@@ -25,6 +25,39 @@ public class TestConsecutiveLetters {
 	inputWords.add("mango");
 	inputWords.add("hello");
 	inputWords.add("letter");
+	
+	
+	// expected output
+	expectedWords.add("apple");
+	expectedWords.add("hello");
+	expectedWords.add("letter");
+	
+	
+	ArrayList<String> actualWords=cl.getConsecutiveLetter(inputWords);
+	
+	AssertJUnit.assertEquals(expectedWords,actualWords);
+	
+    }
+	
+	
+//Test Case 2
+	
+	@Test
+	public void testForCapitalAndSmallLetters() {
+		
+	ConsecutiveLetters cl=new ConsecutiveLetters();
+	
+	ArrayList<String> inputWords=new ArrayList<String>();
+	
+	ArrayList<String> expectedWords=new ArrayList<String>();
+	
+	
+	
+	// actual input
+	inputWords.add("apPle");
+	inputWords.add("mango");
+	inputWords.add("heLlo");
+	inputWords.add("leTTer");
 	
 	
 	// expected output
